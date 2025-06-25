54ca2751ca9a8c0c0e6255ba9c17ba5cda7ceeb2
Divyansh-Kamboj <dkamboj1301@gmail.com>
2025-06-25 22:47:47 +0530

    completed second ds video

---
+++ ds_vid2.py
@@ -0,0 +1,28 @@
+import numpy as np 
+ 
+x = np.array([
+    [
+    [1,2],
+    [4,5],
+    [14,15]
+
+],
+
+[
+    [7,8],
+    [10,11],
+    [12,13]
+
+],
+[
+    [7,8],
+    [10,11],
+    [12,13]
+
+]
+])
+
+print(x)
+print(x.ndim)
+print(x.shape)
+print(x.size)
